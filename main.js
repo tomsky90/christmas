c5731e8cf877dfc56371e74a47e5cbf502c87c83
Tomasz Kaszycki <kaszyckitomasz@interia.pl>
2023-12-05 07:07:08 +0000

    feat: change imgs

--- main.js
+++ main.js
@@ -1,65 +1,62 @@
-import Snow from './snowAnimation.js'
+class ChristmasCounter {
+  htmlElements = {
+    htmlDaysElement: document.querySelector(".days"),
+    htmlHoursElement: document.querySelector(".hours"),
+    htmlMinutesElement: document.querySelector(".minutes"),
+    htmlSecondsElement: document.querySelector(".seconds"),
+    htmlNavBtn: document.querySelector(".nav-btn"),
+    htmlNavBar: document.querySelector(".nav-bar"),
+  };
+  init() {
+    const date = new Date("12/25/2023");
+    const endTime = date.getTime();
+    this.calculateTimeLeft(endTime);
+    this.htmlElements.htmlNavBtn.addEventListener(
+      "click",
+      this.toggleNavBar.bind(this)
+    );
+  }
 
-class ChristmasCounter{
-    
-    htmlElements = {
-        htmlDaysElement: document.querySelector('.days'),
-        htmlHoursElement: document.querySelector('.hours'),
-        htmlMinutesElement: document.querySelector('.minutes'),
-        htmlSecondsElement: document.querySelector('.seconds'),
-        htmlNavBtn: document.querySelector('.nav-btn'),
-        htmlNavBar: document.querySelector('.nav-bar'),
-    }
-    init(){
-        const date = new Date("12/25/2023");
-        const endTime = date.getTime();
-        this.calculateTimeLeft(endTime);
-        this.htmlElements.htmlNavBtn.addEventListener('click', this.toggleNavBar.bind(this))
-    }
+  toggleNavBar() {
+    this.htmlElements.htmlNavBar.classList.toggle("active");
+  }
 
-    toggleNavBar() {
-        this.htmlElements.htmlNavBar.classList.toggle('active')
-    }
+  calculateTimeLeft(time) {
+    setInterval(() => {
+      const timeNow = new Date().getTime();
+      const timeLeft = time - timeNow;
+      this.setDays(timeLeft);
+      this.setHours(timeLeft);
+      this.setMinutes(timeLeft);
+      this.setSeconds(timeLeft);
+    }, 1000);
+  }
 
-    calculateTimeLeft(time) {
-        setInterval(() => {
-            const timeNow = new Date().getTime();
-            const timeLeft = time - timeNow;
-            this.setDays(timeLeft);
-            this.setHours(timeLeft);
-            this.setMinutes(timeLeft);
-            this.setSeconds(timeLeft);
-        }, 1000)
-    }
-
-    setDays(time) {
-        let result = Math.floor((time / (1000 * 60 * 60 * 24)))
-            this.htmlElements.htmlDaysElement.textContent = result.toFixed(0)
-    }
-    setHours(time) {
-        let result = Math.floor((time / (1000 * 60 * 60) % 24));
-        let finalResult = result.toFixed(0)
-        finalResult < 10 ? finalResult = '0' + finalResult: finalResult
-            this.htmlElements.htmlHoursElement.textContent = finalResult
-    }
-    setMinutes(time) {
-        let result = Math.floor(time / (1000 * 60 ) % 60);
-        let finalResult = result.toFixed(0)
-        finalResult < 10 ? finalResult = '0' + finalResult: finalResult
-            this.htmlElements.htmlMinutesElement.textContent = finalResult
-    }
-    setSeconds(time) {
-        let result = Math.floor((time / (1000 ) % 60));
-        let finalResult = result.toFixed(0)
-        finalResult < 10 ? finalResult = '0' + finalResult: finalResult
-            this.htmlElements.htmlSecondsElement.textContent = finalResult
-    }
-    
+  setDays(time) {
+    let result = Math.floor(time / (1000 * 60 * 60 * 24));
+    this.htmlElements.htmlDaysElement.textContent = result.toFixed(0);
+  }
+  setHours(time) {
+    let result = Math.floor((time / (1000 * 60 * 60)) % 24);
+    let finalResult = result.toFixed(0);
+    finalResult < 10 ? (finalResult = "0" + finalResult) : finalResult;
+    this.htmlElements.htmlHoursElement.textContent = finalResult;
+  }
+  setMinutes(time) {
+    let result = Math.floor((time / (1000 * 60)) % 60);
+    let finalResult = result.toFixed(0);
+    finalResult < 10 ? (finalResult = "0" + finalResult) : finalResult;
+    this.htmlElements.htmlMinutesElement.textContent = finalResult;
+  }
+  setSeconds(time) {
+    let result = Math.floor((time / 1000) % 60);
+    let finalResult = result.toFixed(0);
+    finalResult < 10 ? (finalResult = "0" + finalResult) : finalResult;
+    this.htmlElements.htmlSecondsElement.textContent = finalResult;
+  }
 }
 
-window.onload = function(){
-    const counter = new ChristmasCounter()
-    counter.init()
-}
-    
-
+window.onload = function () {
+  const counter = new ChristmasCounter();
+  counter.init();
+};
